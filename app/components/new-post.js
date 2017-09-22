cccf02614c0fc10c2d7b07f87bffd996986c858d
Gitu Mbugua <gmbugua38@gmail.com>
2017-09-22 16:49:52 +0300

    add component to add a question

---
+++ app/components/new-post.js
@@ -0,0 +1,24 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewPost: false,
+  actions: {
+    postFormShow() {
+      this.set('addNewPost', true);
+    },
+
+    cancel() {
+      this.set('addNewPost', false);
+    },
+
+    savePost() {
+      var params = {
+        author: this.get('author'),
+        question: this.get('question'),
+        notes: this.get('notes'),
+      };
+      this.set('addNewPost', false);
+      this.sendAction('savePost', params);
+    }
+  }
+});
