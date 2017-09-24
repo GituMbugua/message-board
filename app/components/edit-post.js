a069444acf6971bb8fa08b4d06c2ebb3c5cdd3ce
Gitu Mbugua <gmbugua38@gmail.com>
2017-09-24 21:06:16 +0300

    add functionality to edit existing questions

---
+++ app/components/edit-post.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  editPostForm: false,
+
+  actions: {
+    editPostForm() {
+      this.set('editPostForm', true);
+    },
+    edit(post) {
+      var params = {
+        title: this.get('title'),
+        author: this.get('author'),
+        question: this.get('question'),
+        notes: this.get('notes'),
+      };
+      this.set('editPostForm', false);
+      this.sendAction('edit', post, params);
+    }
+  }
+});
