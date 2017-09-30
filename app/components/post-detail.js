caf9fd2027ee2212957e59517c173307f89bcbc0
Gitu Mbugua <gmbugua38@gmail.com>
2017-09-30 18:53:33 +0300

    add  common well for answers

--- app/components/post-detail.js
+++ app/components/post-detail.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    edit(post) {
+      var params = {
+        title: this.get('title'),
+        author: this.get('author'),
+        question: this.get('question'),
+        notes: this.get('notes'),
+      };
+      this.set('editPostForm', false);
+      this.sendAction('edit', post, params);
+    }
+  }
 });
