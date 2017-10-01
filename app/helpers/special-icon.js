20f7790f38629057646b546926c6a10386c554b8
Gitu Mbugua <gmbugua38@gmail.com>
2017-10-01 16:54:25 +0300

    add icon for questions with more than 5 answers

--- app/helpers/special-icon.js
+++ app/helpers/special-icon.js
@@ -5,6 +5,8 @@ export function specialIcon(params/*, hash*/) {
 
   if (post.get('answers').get('length') === 0) {
     return Ember.String.htmlSafe('<span class="glyphicon glyphicon-exclamation-sign" data-toggle="tooltip" title="This question has no answers"></span>');
+  } else if (post.get('answers').get('length') >= 5) {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-star-empty" data-toggle="tooltip" title="This question has more than 5 answers"></span>');
   }
 }
 
